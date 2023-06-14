b316165bc08def5dc0fe31d72c4fa1e4b6951bf0
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-14 06:24:33 -0500

    Initial Commit

---
+++ SwiftChartsPieDonut/SwiftChartsPieDonutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsPieDonutApp.swift
+//  SwiftChartsPieDonut
+//
+//  Created by Mark Volkmann on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsPieDonutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
